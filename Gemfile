40ca8f155f12262dfedf055a91da7fe6b60322c7
Helena Becker <helena.becker1104@gmail.com>
2023-11-14 16:56:27 +0100

    wikinimous

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
+gem 'faker'
+
 
 ruby "3.1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
+  get "/articles/:id/edit", to:"articles#edit", as: :edit_article
+  get "articles/:id", to: "articles#show", as: :article
+  patch "/articles/:id", to: "articles#update", as: :update_article
+  delete "articles/:id", to: "articles#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
